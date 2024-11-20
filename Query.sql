f7439614a082f3794418c71d6a7756aad8c11bcc
Thang0226 <thang.nd0226@gmail.com>
2024-11-20 13:27:22 +0700

    beautify queries

--- Query.sql
+++ Query.sql
@@ -1,52 +1,64 @@
-use QuanLySinhVien;
+USE QuanLySinhVien;
 
-insert into Class values
+INSERT INTO Class VALUES
 	(1, 'A1', '2008-12-20', 1),
 	(2, 'A2', '2008-12-22', 1),
-	(3, 'B3', date(now()), 1);
+	(3, 'B3', DATE(NOW()), 1);
     
-insert into Class values (3, 'B3', date(now()), 1);
+INSERT INTO Class VALUES (3, 'B3', DATE(NOW()), 1);
     
-select * from Class;
+SELECT 
+    *
+FROM
+    Class;
 
-delete from Class 
-	where (ClassID > 2);
+DELETE FROM Class 
+WHERE
+    (ClassID > 2);
     
 
 
 
 
-insert into Student values (1, 'Hung', 'Ha noi', '0912113113', 1, 1);
-insert into Student (StudentName, Address, Status, ClassID) values 
+INSERT INTO Student VALUES (1, 'Hung', 'Ha noi', '0912113113', 1, 1);
+INSERT INTO Student (StudentName, Address, Status, ClassID) VALUES 
 	('Hoa', 'Hai phong', 1, 1);
-insert into Student (StudentName, Address, Phone, Status, ClassID) values
+INSERT INTO Student (StudentName, Address, Phone, Status, ClassID) VALUES
 	('Manh', 'HCM', '0123123123', 0, 2);
     
-select * from Student;
+SELECT 
+    *
+FROM
+    Student;
 
 
 
 
 
-insert into Subject(SubName, Credit, Status) values 
+INSERT INTO Subject(SubName, Credit, Status) VALUES 
 	('CF', 5, 1),
 	('C', 6, 1),
 	('HDJ', 5, 1),
 	('RDBMS', 10, 1);
     
-select * from Subject;
+SELECT 
+    *
+FROM
+    Subject;
 
-delete from Subject where (SubID > 0);
+DELETE FROM Subject 
+WHERE
+    (SubID > 0);
 
-alter table Subject auto_increment = 0;
+ALTER TABLE Subject AUTO_INCREMENT = 0;
 
 
 
 
 
-insert into Mark (SubID, StudentID, Mark, ExamTimes) values
+INSERT INTO Mark (SubID, StudentID, Mark, ExamTimes) VALUES
 	(1, 1, 8, 1),
     (1, 2, 10, 2),
     (2, 1, 12, 1);
     
-select * from Mark;
+SELECT * FROM Mark;
